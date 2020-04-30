1594168bee9d317544baa722aaca16a9a85e5511
Taroelli <sergio.roelli@gmail.com>
2020-04-30 17:36:35 +0200

    CCS Setup complete

---
+++ 02_MSP430/OutOfBox/Debug/StopWatchMode.d
@@ -0,0 +1,89 @@
+# FIXED
+
+StopWatchMode.obj: ../StopWatchMode.c
+StopWatchMode.obj: ../StopWatchMode.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr4133.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/in430.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics_legacy_undefs.h
+StopWatchMode.obj: ../hal_LCD.h
+StopWatchMode.obj: ../main.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/driverlib.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/inc/hw_memmap.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdint.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/_stdint40.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/stdint.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/cdefs.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_types.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_types.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_stdint.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_stdint.h
+StopWatchMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdbool.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/adc.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/crc.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/cs.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/ecomp.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_a_spi.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_a_uart.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_b_i2c.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_b_spi.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/framctl.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/gpio.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/lcd_e.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/mpy32.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/pmm.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/rtc.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sac.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sfr.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sysctl.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/tia.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/timer_a.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/timer_b.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/tlv.h
+StopWatchMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/wdt_a.h
+
+../StopWatchMode.c: 
+../StopWatchMode.h: 
+/home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr4133.h: 
+/home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/in430.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics_legacy_undefs.h: 
+../hal_LCD.h: 
+../main.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/driverlib.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/inc/hw_memmap.h: 
+/home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h: 
+/home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdint.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/_stdint40.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/stdint.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/cdefs.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_types.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_types.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_stdint.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_stdint.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdbool.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/adc.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/crc.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/cs.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/ecomp.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_a_spi.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_a_uart.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_b_i2c.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_b_spi.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/framctl.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/gpio.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/lcd_e.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/mpy32.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/pmm.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/rtc.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sac.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sfr.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sysctl.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/tia.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/timer_a.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/timer_b.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/tlv.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/wdt_a.h: 
---
+++ 02_MSP430/OutOfBox/Debug/TempSensorMode.d
@@ -0,0 +1,89 @@
+# FIXED
+
+TempSensorMode.obj: ../TempSensorMode.c
+TempSensorMode.obj: ../TempSensorMode.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr4133.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/in430.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics_legacy_undefs.h
+TempSensorMode.obj: ../hal_LCD.h
+TempSensorMode.obj: ../main.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/driverlib.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/inc/hw_memmap.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdint.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/_stdint40.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/stdint.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/cdefs.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_types.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_types.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_stdint.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_stdint.h
+TempSensorMode.obj: /home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdbool.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/adc.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/crc.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/cs.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/ecomp.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_a_spi.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_a_uart.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_b_i2c.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_b_spi.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/framctl.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/gpio.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/lcd_e.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/mpy32.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/pmm.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/rtc.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sac.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sfr.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sysctl.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/tia.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/timer_a.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/timer_b.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/tlv.h
+TempSensorMode.obj: /home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/wdt_a.h
+
+../TempSensorMode.c: 
+../TempSensorMode.h: 
+/home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr4133.h: 
+/home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/in430.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics_legacy_undefs.h: 
+../hal_LCD.h: 
+../main.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/driverlib.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/inc/hw_memmap.h: 
+/home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h: 
+/home/sergio/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdint.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/_stdint40.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/stdint.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/cdefs.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_types.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_types.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_stdint.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_stdint.h: 
+/home/sergio/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdbool.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/adc.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/crc.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/cs.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/ecomp.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_a_spi.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_a_uart.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_b_i2c.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/eusci_b_spi.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/framctl.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/gpio.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/lcd_e.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/mpy32.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/pmm.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/rtc.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sac.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sfr.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/sysctl.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/tia.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/timer_a.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/timer_b.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/tlv.h: 
+/home/sergio/PrgC2020FSSergioRoelli/02_MSP430/DriverLib/wdt_a.h: 
